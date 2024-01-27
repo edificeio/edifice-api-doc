5e9265d576271b45f0490329e8b08d91a9286032
Rafik Djedjig <rafik.djedjig@web-education.net>
2024-01-27 17:16:27 +0100

    chore: Refactor global structure to avoid code duplication and ease swagger-ui's dependency version update

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,16 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.json",
+    // url: "swagger.json",
+    urls : [
+      {url: "./api-definition/auth.yaml", name: "Auth"},
+      {url: "./api-definition/directory.yaml", name: "Directory"},
+      {url: "./api-definition/communication.yaml", name: "Communication"},
+      {url: "./api-definition/timeline.yaml", name: "Timeline"},
+      {url: "./api-definition/explorer.yaml", name: "Explorer"},
+      {url: "./api-definition/conversation.yaml", name: "Conversation"},
+      {url: "./api-definition/actualites.yaml", name: "Actualites"}
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
