948eec176ccc11177278ce56931e9c9abc9fd751
moustaphahennawi <56674781+moustaphahennawi@users.noreply.github.com>
2024-02-28 19:59:16 +0100

    chore: IW-24 add module Reservation booking system (RBS)

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,29 +1,28 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "swagger.json",
-    urls : [
-      {url: "./api-definition/auth.yaml", name: "Auth"},
-      {url: "./api-definition/directory.yaml", name: "Directory"},
-      {url: "./api-definition/communication.yaml", name: "Communication"},
-      {url: "./api-definition/timeline.yaml", name: "Timeline"},
-      {url: "./api-definition/explorer.yaml", name: "Explorer"},
-      {url: "./api-definition/conversation.yaml", name: "Conversation"},
-      {url: "./api-definition/actualites.yaml", name: "Actualites"},
-      {url: "./api-definition/infra.yaml", name: "Infra"}
+    urls: [
+      { url: "./api-definition/auth.yaml", name: "Auth" },
+      { url: "./api-definition/directory.yaml", name: "Directory" },
+      { url: "./api-definition/communication.yaml", name: "Communication" },
+      { url: "./api-definition/timeline.yaml", name: "Timeline" },
+      { url: "./api-definition/explorer.yaml", name: "Explorer" },
+      { url: "./api-definition/conversation.yaml", name: "Conversation" },
+      { url: "./api-definition/actualites.yaml", name: "Actualites" },
+      { url: "./api-definition/infra.yaml", name: "Infra" },
+      {
+        url: "./api-definition/rbs.yaml",
+        name: " Reservation booking system (RBS)",
+      },
     ],
-    dom_id: '#swagger-ui',
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
